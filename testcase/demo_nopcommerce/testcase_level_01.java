61e9049e636ec77adc5133e7cd63e0add9fa89d9
Gin <oanhdang1604@gmail.com>
2025-10-11 21:55:07 +0700

    update

--- testcase/demo_nopcommerce/testcase_level_01.java
+++ testcase/demo_nopcommerce/testcase_level_01.java
@@ -1,6 +1,8 @@
 package demo_nopcommerce;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -8,7 +10,9 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class testcase_level_01 {
+import static demo_nopcommerce.pageUILocator.*;
+
+public class testcase_level_01 extends commons {
     WebDriver driver;
 
     @BeforeClass
@@ -19,18 +23,28 @@ public class testcase_level_01 {
     }
 
     @Test
-    public void TC_01_RegisterUser(){
-
+    public void TC_01_LoginUser(){
+        getWebElement(driver, login_button).click();
+        sendKeyToElement(driver, email,"gindang123@gmail.com");
+        sendKeyToElement(driver, password,"123456");
+        getWebElement(driver,login).click();
     }
 
     @Test
     public void TC_02_VerifyInfo(){
+        getWebElement(driver, myAccount).click();
+
+        //driver.switchTo().parentFrame(("https://demo.nopcommerce.com/customer/info"));
+        getWebElement(driver, addresses).click();
+        getWebElement(driver,addNewAddress).click();
+        selectItemDropDownDefault(driver, dropdownCountry,"Vietnam");
+
 
     }
 
     @AfterClass
     public void afterClass(){
-        driver.quit();
+        //driver.quit();
     }
 
 }
